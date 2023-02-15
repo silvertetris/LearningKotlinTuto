136b1880ad0e608bbb41d0b0fd51be4ccc535f0b
silvertetris <dltjdrnjs3@naver.com>
2023-02-15 14:15:33 +0900

    thread
    should learn thread

---
+++ src/chap03/CallByValue.kt
@@ -0,0 +1,17 @@
+package chap03
+
+
+fun main() {
+    val result = callByValue(lambda() )
+    println(result)
+}
+
+fun callByValue(b:Boolean): Boolean{
+    println("callByValue function")
+    return b
+}
+
+val lambda: () -> Boolean={
+    println("lambda function")
+    true
+}
---
+++ src/chap03/LockHighOrder.kt
@@ -0,0 +1,27 @@
+package chap03
+
+import java.util.concurrent.locks.ReentrantLock
+
+var sharable=1
+
+fun main() {
+    val reLock = ReentrantLock()
+    lock(reLock, { criticalFunc()})
+    lock(reLock) {criticalFunc() }
+    lock(reLock, ::criticalFunc)
+
+    println(sharable)
+}
+
+fun criticalFunc() {
+    sharable +=1
+}
+
+fun<T> lock(reLock: ReentrantLock, body: () ->T): T{
+    reLock.lock()
+    try{
+        return body()
+    } finally {
+        reLock.unlock()
+    }
+}
