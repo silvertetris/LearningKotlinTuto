cc2f9cc37a448daacc312bc5d92c85f11d65b11c
silvertetris <dltjdrnjs3@naver.com>
2023-03-24 21:49:17 +0900

    124124124

---
+++ src/chap03/InlineFunction.kt
@@ -0,0 +1,16 @@
+package chap03
+
+fun main() {
+    shortFunc(3) {
+        println("First call: $it")
+    }
+    shortFunc(5) {
+        println("Second call: $it")
+    }
+}
+
+inline fun shortFunc(a: Int, out: (Int) -> Unit) {
+    println("Before calling out( )")
+    out(a)
+    println("After calling out( )")
+}
---
+++ src/chap03/LocalReturn.kt
@@ -0,0 +1,8 @@
+package chap03
+
+fun main() {
+    shortFunc(3) {
+        println("First call: $it")
+        return
+    }
+}
