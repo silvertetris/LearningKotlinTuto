11b76333b9450c26bbac11d3a7c3a9cdf71c8d86
silvertetris <dltjdrnjs3@naver.com>
2023-03-26 11:23:27 +0900

    Update TailRecFactorial.kt

--- src/chap03/TailRecFactorial.kt
+++ src/chap03/TailRecFactorial.kt
@@ -5,6 +5,6 @@ fun main() {
     println("Factorial: $number-> ${factorial(number)}")
 }
 
-tailrec fun factorial(n: Int, run: Int=1): Long {
+tailrec fun factorial(n: Int, run: Int=1): Long { //tailrec 스택 재활용, 스택 오버플로우 x (메모리)
     return if(n==1) run.toLong() else factorial(n-1, run*n)
 }
