4189fe57392ac974ef784496a115e9dcbdecedb9
silvertetris <dltjdrnjs3@naver.com>
2023-03-28 00:45:41 +0900

    Create CustomGetterSetter.kt

---
+++ src/main/kotlin/chap6/CustomGetterSetter.kt
@@ -0,0 +1,19 @@
+package chap6
+
+import java.util.*
+
+class User(_id: Int, _name: String, _age: Int) {
+    val id: Int=_id
+    var name: String = _name
+        set(value) {
+            println("The name was changed")
+            field= value.uppercase(Locale.getDefault())
+        }
+    var age: Int=_age
+}
+
+fun main() {
+    val user1=User(1, "Kildong", 35)
+    user1.name="coco"
+    println("user3.name=${user1.name}")
+}
