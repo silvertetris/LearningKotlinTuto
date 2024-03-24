c4de5b934e99ed639bf4171484470e905ce8f93c
silvertetris <dltjdrnjs3@naver.com>
2024-03-25 06:14:36 +0900

    Update Acheivements.kt

--- src/main/kotlin/baekjoon/Acheivements.kt
+++ src/main/kotlin/baekjoon/Acheivements.kt
@@ -24,27 +24,29 @@ fun main() {
     }
     sc.close()
 
-    /*fun checkBigger(currentStreakLength: Int, paid:Int) : Int{
-        if (currentStreakLength>paid) {
-            return paid-1
-        }
-        return currentStreakLength
-    }*/
     fun findLongestStreak(practiced: Int, paid: Int, actualDays: IntArray): Int {
         var longestStreakLength = 0//최대 길이
-        var startPoint = 0//비교 시작지점
-        for (i in 0 until practiced - 1) {
+        //var startPoint = 0//비교 시작지점
+        for(startPoint in 0 until practiced-1){
+            var remainPaid = paid //남아있는 지불 기회
+            var temp=0
 
-
-            var currentStreakLength = actualDays[i + 1] - actualDays[startPoint] + 1 //지불 여부 상관없이 연속일 수
-            var paidDays = minOf(currentStreakLength - 2, paid)//실제 지불한 일 수 , paid가 최대가 되어야함으로 minOf를 통해 구함
-            currentStreakLength -= paidDays // 지불일수를 제외한 연속 일 수
-            //currentStreakLength=checkBigger(currentStreakLength, paid)
-            if (actualDays[i + 1] - actualDays[i] > paid) {
-                startPoint = i
-                longestStreakLength= maxOf(longestStreakLength, paid+1)
-            } else {
-                longestStreakLength = maxOf(longestStreakLength, currentStreakLength + paid)
+            for (i in startPoint until practiced - 1) {
+                if((actualDays[i+1]-actualDays[i]-1)>paid) {//차이가 paid보다 클 경우 로직.
+                    longestStreakLength= maxOf(longestStreakLength, paid+1)
+                    startPoint+1
+                    break
+                }
+                var currentStreakLength = actualDays[i + 1] - actualDays[i] + 1 //지불 여부 상관없이 연속일 수
+                var actualPaid = minOf(currentStreakLength - 2, paid)//실제 지불한 일 수 , paid가 최대가 되어야함으로 minOf를 통해 구함
+                currentStreakLength -= remainPaid // 지불일수를 제외한 연속 일 수
+                remainPaid -=actualPaid//사용한 지불 횟수 ( 남은 지불 횟수)
+                if(actualDays[i+1]-actualDays[i]-1>remainPaid&&remainPaid==0) {
+                    longestStreakLength++
+                    startPoint+1
+                    break
+                }
+                longestStreakLength= maxOf(longestStreakLength, currentStreakLength+actualPaid+remainPaid) //기존 , 제외한 연속 일수 + 실제 지불 수 , 최대값 구하기
             }
         }
         return longestStreakLength
