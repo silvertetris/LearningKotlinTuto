9b62d1bad3ddc7ab83d6e5b8c659e454f5808851
CAPYBARA <dltjdrnjs3@naver.com>
2024-11-27 19:57:17 +0900

    Update Acheivements.kt

--- src/main/kotlin/baekjoon/Acheivements.kt
+++ src/main/kotlin/baekjoon/Acheivements.kt
@@ -1,56 +1,32 @@
 package baekjoon
 
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.InputStreamReader
-import java.io.OutputStreamWriter
-
-lateinit var actualDays: IntArray //지연 선언
+import java.util.*
 
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val p = sc.nextInt()
+    val actualDays = IntArray(n) { sc.nextInt() }
 
-    var longestStreakLength = 0//최대 길이
-    var startPoint = 0
-    var practiced: Int
-    var paid: Int
-    do {
-        val (p, q) = br.readLine().split(" ").map { it.toInt() }
-        practiced = p
-        paid = q
-    } while (practiced !in 1..200000 || paid !in 1..200000)
-    actualDays = IntArray(practiced)
-    var num = 0
-    do {
-        val days = br.readLine().split(" ").map { it.toInt() }
-        for (i in 0 until practiced) {
-            actualDays[i] = days[i]
-            num++
+    var maxStreak = 0
+    var remainPaid = 0
+    var start = 0
+
+    for (end in actualDays.indices) {
+        if (end > 0) {
+            val gap = actualDays[end] - actualDays[end - 1] - 1
+            remainPaid += gap
         }
-    } while (actualDays[num - 1] !in 0..1000000)
 
-    //begins logic
-    var endReached = false
-    while (startPoint <= practiced - 2 && !endReached) {
-        var remainPaid = paid //남아있는 지불 기회 startPoint가 올라갈떄마다 remainPaid는 초기화 되어야함.
-        for (i in startPoint + 1 until practiced) {
-            val gap = actualDays[i] - actualDays[i - 1] - 1 //차이
-            if(remainPaid<gap) { //차이가 지불일수를 넘을 경우
-                if(i==startPoint+1) {//1. 처음부터 넘을 경우 (최대 길이가 0이기 때문에 정해줌)
-                    longestStreakLength= maxOf(longestStreakLength, paid+1)
-                }
-                break // 2. 끝자락에서 넘을 경우 (i를 올릴 필요가 없기에 탈출)
-            }
+
+        while (remainPaid > p) {
+            val gap = actualDays[start + 1] - actualDays[start] - 1
             remainPaid -= gap
-            longestStreakLength = maxOf(longestStreakLength, i - startPoint + paid + 1)
-            if(i==practiced-1) { //i가 끝자락에 도달하면 아우터 반복문까지 탈출 (스타트 포인트를 옮길 필요 x)
-                endReached=true
-            }
+            start++
         }
-        startPoint++
+
+        maxStreak = maxOf(maxStreak, actualDays[end] - actualDays[start] + 1 + p - remainPaid)
     }
-    bw.write("$longestStreakLength")
-    bw.flush()
-    bw.close()
+
+    println(maxStreak)
 }
