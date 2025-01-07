13579fdc8d0e43f602c5eeba030abdf1164efb92
CAPYBARA <dltjdrnjs3@naver.com>
2025-01-07 20:27:53 +0900

    Update IncomeInequality.kt

--- src/main/kotlin/baekjoon/IncomeInequality.kt
+++ src/main/kotlin/baekjoon/IncomeInequality.kt
@@ -4,24 +4,24 @@ import kotlin.math.abs
 fun main() {
     val sc = Scanner(System.`in`)
 
-    val n = sc.nextInt()
-    val input: Array<Float> = Array(n) { 0.0f }
+    val n = sc.nextInt()//integer
+    val input: Array<Double> = Array(n) { 0.0 }
     for (i in 0 until n) {
-        input[i] = sc.nextFloat()
+        input[i] = sc.nextDouble()
     }
 
     val sum = input.sum()
     val avgProperty: Double = sum.toDouble() / n
-    var bigSum: Int = 0
-    var bigNum: Int = 0
+    var bigSum = 0.0 //큰 놈들의 합
+    var bigNum: Int = 0 //큰 놈들의 수
     for (i in 0 until n) {
         if (input[i] > avgProperty) {
-            bigSum += input[i].toInt()
+            bigSum += input[i]
             bigNum++
         }//평균보다 많은 놈 도출
     }
-    val y = (bigSum.toDouble() / sum) * 100
-    val x: Double = (bigNum.toDouble() / n) * 100
-    val differ = abs(y - x)
+    val y = (bigSum / sum)
+    val x: Double = (bigNum.toDouble() / n)
+    val differ = (y - x)*100
     println(String.format("%.6f", differ))
 }
