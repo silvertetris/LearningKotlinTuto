17c9afa1f9ac9b0a4883c189e64fa752acc9ae20
silvertetris <dltjdrnjs3@naver.com>
2025-01-13 22:51:01 +0900

    Create MusicalChairs.kt

---
+++ src/main/kotlin/baekjoon/MusicalChairs.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+import java.util.TreeMap
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    val arr:ArrayList<TreeMap<Int, Int>> = ArrayList<TreeMap<Int, Int>>()
+    repeat(n) {
+        var num = sc.nextInt()
+
+    }
+
+}
