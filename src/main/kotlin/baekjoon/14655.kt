621eb9ef6e9a4df50b243e282b2473320079d87a
silvertetris <dltjdrnjs3@naver.com>
2025-01-16 03:01:45 +0900

    Create 14655.kt

---
+++ src/main/kotlin/baekjoon/14655.kt
@@ -0,0 +1,12 @@
+import java.util.*
+import kotlin.math.abs
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    var result = 0
+    repeat(n*2) {
+        result += abs(sc.nextInt())
+    }
+    println(result)
+}
