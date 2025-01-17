395e6ed1783403c890dcbc9130fd5d587bbdd34f
CAPYBARA <dltjdrnjs3@naver.com>
2025-01-17 15:33:21 +0900

    Update Bubbles.kt

--- src/main/kotlin/baekjoon/MusicalChairs.kt
+++ src/main/kotlin/baekjoon/MusicalChairs.kt
@@ -1,13 +1,27 @@
 import java.util.Scanner
-import java.util.TreeMap
 
 fun main() {
     val sc = Scanner(System.`in`)
     val n = sc.nextInt()
-    val arr:ArrayList<TreeMap<Int, Int>> = ArrayList<TreeMap<Int, Int>>()
-    repeat(n) {
-        var num = sc.nextInt()
-
+    var composeList: ArrayList<Array<Int>> = ArrayList<Array<Int>>() //0 자리 = 교수 순번, 1자리 = 최애곡 모음
+    for (i in 0 until n) {
+        composeList.add(arrayOf(i, sc.nextInt()))
     }
-
+    var startIndex = 0
+    while (composeList.size != 1) {
+        var shouldIterate = (composeList.get(startIndex).get(1) - 1) % composeList.size
+        var deleteIndex: Int
+        if(startIndex+shouldIterate>composeList.size-1) {
+            deleteIndex=startIndex+shouldIterate-composeList.size
+        } else{
+            deleteIndex=startIndex+shouldIterate
+        }
+        composeList.removeAt(deleteIndex)
+        if(deleteIndex>composeList.size-1){
+            startIndex = 0
+        } else{
+            startIndex = deleteIndex
+        }
+    }
+    println(composeList.get(0).get(0)+1)
 }
