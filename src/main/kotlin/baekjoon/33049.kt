0420446fdefa91114e36cbfbd7956139d637faf3
CAPYBARA <dltjdrnjs3@naver.com>
2025-01-28 18:24:29 +0900

    Update 33049.kt

--- src/main/kotlin/baekjoon/33049.kt
+++ src/main/kotlin/baekjoon/33049.kt
@@ -3,43 +3,48 @@ fun main() {
     val p3 = toTypedArray[0]
     val p4 = toTypedArray[1]
     var p0 = toTypedArray[2]
-    var triple =0
+    var triple = 0
     var quad = 0
 
-    triple+= p3/3
-    quad+= p4/4
-    var tripleRemain = p3%3
-    var quadRemain = p4%4
-    if(p0 - (3-tripleRemain + 4-quadRemain) < 0) {
+    triple += p3 / 3
+    quad += p4 / 4
+    var tripleRemain = p3 % 3
+    var quadRemain = p4 % 4
+    if(tripleRemain != 0) {
+        triple++
+        p0 = p0 - 3 +tripleRemain
+    }
+    if(quadRemain != 0) {
+        quad++
+        p0 = p0 -4 +quadRemain
+    }
+    if(p0<0||p0==1 || p0 ==2 ||p0==5) {
         print(-1)
         return
     }
-    while(true) {
-        if(tripleRemain%3!=0) {
-            tripleRemain++
-            p0--
+    while(p0!=0) {
+        p0-=4
+        quad++
+        if(p0<0) {
+            quad--
+            p0+=1
+            triple++
         }
-        if(quadRemain%4!=0) {
-            quadRemain++
-            p0--
+        else if(p0==3) {
+            triple++
+            break
         }
-        if(tripleRemain%3==0 && quadRemain%4==0) {
+        else if(p0==2) {
+            triple+=2
+            quad-=1
+            break
+        }
+        else if(p0==1) {
+            triple+=3
+            quad-=2
             break
         }
     }
-    var maxX=-1
-    for (y in 0..p0 / 3) {
-        val remainder = p0 - 3 * y
-        if (remainder % 4 == 0) {
-            val x = remainder / 4
-            maxX = maxOf(maxX, x)
-        }
-    }
-    if(maxX==-1) {
-        print(-1)
-        return
-    }
-    quad+=maxX
-    triple+=(p0-(maxX*4))/3
+
     print("$triple $quad")
 }
