34e8bb374e5f4a297e9e91f1ed1aec1f5acd539d
silvertetris <dltjdrnjs3@naver.com>
2025-02-01 23:52:09 +0900

    Create 10773.kt

---
+++ src/main/kotlin/baekjoon/10773.kt
@@ -0,0 +1,15 @@
+import java.util.Stack
+
+fun main() {
+    val k = readln().toInt()
+    var result= Stack<Long>()
+    repeat(k) {
+        val input = readln().toInt()
+        if(input==0) {
+            result.pop()
+        }else {
+            result.push(input.toLong())
+        }
+    }
+    println(result.sum())
+}
