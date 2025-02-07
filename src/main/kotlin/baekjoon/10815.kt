e832f367a55d23b7897b576ad45151b19c013f2f
silvertetris <dltjdrnjs3@naver.com>
2025-02-07 16:12:16 +0900

    Create 10815.kt

---
+++ src/main/kotlin/baekjoon/10815.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val n = readln().toInt()
+    var have = readln().split(" ").map { it.toInt() }.toMutableList()
+    val m = readln().toInt()
+    var find = readln().split(" ").map { it.toInt() }.toMutableList()
+    have.sort()
+    find.sort()
+    
+}
