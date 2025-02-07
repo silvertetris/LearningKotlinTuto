3c887ca07511914acfd57c5711c3ed541d5ebca6
silvertetris <dltjdrnjs3@naver.com>
2025-02-08 01:20:14 +0900

    Create 1015.kt

---
+++ src/main/kotlin/baekjoon/1015.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val n = readln().toInt()
+    val a = readln().split(" ").map { it.toInt() }.toMutableList()
+    var b = a.sorted().toMutableList()
+    var result = Array<Int>(n){0}
+    for(i in 0 until n) {
+        for(j in 0 until n) {
+            if(b[j]==a[i]) {
+                result[i] = j
+                b[j]=0
+                break
+            }
+        }
+    }
+    print(result.joinToString(" "))
+}
