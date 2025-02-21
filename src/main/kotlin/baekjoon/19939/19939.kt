c6596e15f70df2820e9847770a0cee151a1d9cfe
CAPYBARA <dltjdrnjs3@naver.com>
2025-02-21 21:56:43 +0900

    2/21

---
+++ src/main/kotlin/baekjoon/19939/19939.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var (n, k) = readln().split(" ").map{it.toInt()}
+    val minCi = if(k%2==0) {
+        (k/2)*(k+1)
+    } else {
+        k/2*(k+1) +(k/2 +1)
+    }
+    if(minCi>n) {
+        print(-1)
+    }else {
+        n-=minCi
+        if(n%k==0) {
+            print(k-1)
+        }else {
+            print(k)
+        }
+    }
+}
---
+++ src/main/kotlin/baekjoon/7304/7304.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val t = readln().toInt()
+    val result = StringBuilder()
+    repeat(t) {
+        readln()
+        val n = readln().split(' ').map { it.toInt() }.sortedDescending()
+        var temp = 0
+
+        for(i in n.indices) {
+                temp = maxOf(n[i]*(i+1), temp)
+        }
+        result.append("$temp\n")
+    }
+    print(result)
+}
