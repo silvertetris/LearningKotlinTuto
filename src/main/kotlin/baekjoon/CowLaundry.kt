3ee75833f87298c8d7bec6a220eee24d4b9661e2
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-08 19:24:47 +0900

    3/8

--- src/main/kotlin/baekjoon/CowLaundry.kt
+++ src/main/kotlin/baekjoon/CowLaundry.kt
@@ -1,25 +1,13 @@
-import java.util.*
-
 fun main() {
-    val sc = Scanner(System.`in`)
-    val n = sc.nextInt()
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toInt()
     var result = 0
-    var list: ArrayList<Pair<Int, Int>> = ArrayList<Pair<Int, Int>>()
-    repeat(n) {
-        list.add(Pair(sc.nextInt(), sc.nextInt()))
-    }
 
-    list.sortBy { it.first }
-    while (list[0].first != list[0].second) {
-        if (list[0].first < list[0].second) { //top 이 bottom 보다 작을 때
-            val sub = list[0].second - list[0].first
-            result += sub
-            for (j in list[0].first until list[0].second) {
-                list[j] = Pair(list[j].first - 1, list[j].second)
-            }
-            list[0] = Pair(list[0].second, list[0].second)
+    repeat(n) {
+        val(a, b) = br.readLine().split(" ").map { it.toInt() }
+        if(a>b) {
+            result+=a-b
         }
-        list.sortBy { it.first }
     }
-    println(result)
+    print(result)
 }
