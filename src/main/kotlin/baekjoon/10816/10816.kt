a1ac67cb06399fbb70fb45defb169fff1929d418
silvertetris <dltjdrnjs3@naver.com>
2025-03-12 16:32:42 +0900

    Create 10816.kt

---
+++ src/main/kotlin/baekjoon/10816/10816.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    val n = br.readLine().toInt()
+    val have = br.readLine().split(" ").map { it.toInt() }
+    val m = br.readLine().toInt()
+}
