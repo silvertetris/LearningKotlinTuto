22c8f946561ef55a2969f8a8255fb07a659017ad
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-22 19:52:06 +0900

    Update 25972.kt

--- src/main/kotlin/baekjoon/25972/25972.kt
+++ src/main/kotlin/baekjoon/25972/25972.kt
@@ -1,28 +1,20 @@
 fun main() {
-    val br= System.`in`.bufferedReader()
+    val br = System.`in`.bufferedReader()
     val n = br.readLine().toInt()
     val a = Array(n) { Pair(0, 0) }
-    for(i in 0 until n) {
-        var temp = br.readLine().split(" ").map { it.toInt() }
+    for (i in 0 until n) {
+        val temp = br.readLine().split(" ").map { it.toInt() }
         a[i] = Pair(temp[0], temp[1])
     }
     a.sortBy { it.first }
-    if(n==1) {
-        print(1)
-        return
-    }
     var answer = 0
-    var remain = 0
+    var remain = -1
 
-    for(i in 1 until n) {
-        if(a[i].first-a[i-1].second <=remain) {
-            continue
+    for (i in 0 until n) {
+        if(remain< a[i].first) {
+            answer++
         }
-        remain = a[i].first-a[i-1].first
-        if(a[i].first-a[i-1].first > a[i].second) {
-            answer ++
-            continue
-        }
+        remain = a[i].first + a[i].second
     }
-    print(answer+1)
+    print(answer)
 }
