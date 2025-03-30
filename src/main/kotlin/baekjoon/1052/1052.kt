262a2b9112c686dc9e72ee672fc314d52a5fba2a
CAPYBARA <dltjdrnjs3@naver.com>
2025-03-30 21:05:04 +0900

    Create 1052.kt

---
+++ src/main/kotlin/baekjoon/1052/1052.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var (n, k) = readln().split(" ").map { it.toInt() }
+    if (k >= n) {
+        print(0)
+        return
+    } else {
+        var answer = 0
+        while (true) {
+            var cnt = Integer.toBinaryString(n).count { it == '1' }
+            if (k >= cnt) break
+            n++
+            answer++
+        }
+        print(answer)
+    }
+}
