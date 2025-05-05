b42563cf8c3c5ec460c2adc19e4aae7e3eb83bd0
silvertetris <dltjdrnjs3@naver.com>
2025-05-05 23:36:54 +0900

    1

---
+++ src/main/kotlin/baekjoon/26099/26099.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val n = readln().toLong()
+    var answer = -1L
+    var fiveBag = n / 5
+
+    while (fiveBag >= 0) {
+        val rest = n - fiveBag * 5
+        if (rest % 3 == 0L) {
+            answer = fiveBag + (rest / 3)
+            break
+        }
+        fiveBag--
+    }
+    println(answer)
+}
